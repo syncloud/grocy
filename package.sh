4b7b101e61d8535f1fe3d496443fe93f7ea19d46
Boris Rybalkin <ribalkin@gmail.com>
2025-08-03 21:41:11 +0100

    fix

--- package.sh
+++ package.sh
@@ -19,7 +19,6 @@ apt -y install squashfs-tools
 
 cp -r ${DIR}/bin ${SNAP_DIR}
 cp -r ${DIR}/config ${SNAP_DIR}
-cp -r ${DIR}/hooks ${SNAP_DIR}
 cp ${DIR}/snap.yaml ${SNAP_DIR}/meta
 
 echo "version: $VERSION" >> ${SNAP_DIR}/meta/snap.yaml
